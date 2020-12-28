15634c173f6dd1232067391685ae96749f758eb1
Joshua Ramczyk <joswar84@gmail.com>
2020-12-28 12:53:23 -0600

    Added report filter

--- Pages/Reports/ComputerReport.cshtml.cs
+++ Pages/Reports/ComputerReport.cshtml.cs
@@ -2,9 +2,12 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace JGR_WGU_Capstone.Pages.Reports
@@ -19,16 +22,39 @@ namespace JGR_WGU_Capstone.Pages.Reports
             _context = context;
         }
 
+        public SelectList Makes { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CompMake { get; set; }
+        public SelectList Models { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CompModel { get; set; }
         [ViewData]
         public string CurrentDate { get; set; }
-
-        public IList<Computer> Computer { get;set; }
+        public IList<Computer> Computer { get; set; }
 
         public async Task OnGetAsync()
-        {            
-            var TodayDateTime = DateTime.Today;            
+        {
+            IQueryable<string> makeQuery = from c in _context.Computer orderby c.Manufacturer select c.Manufacturer;
+            IQueryable<string> modelQuery = from c in _context.Computer orderby c.Model select c.Model;
+
+            var computers = from c in _context.Computer select c;
+
+            if (!string.IsNullOrEmpty(CompMake))
+            {
+                computers = computers.Where(x => x.Manufacturer == CompMake);
+            }
+
+            if (!string.IsNullOrEmpty(CompModel))
+            {
+                computers = computers.Where(x => x.Model == CompModel);
+            }
+
+            Makes = new SelectList(await makeQuery.Distinct().ToListAsync());
+            Models = new SelectList(await modelQuery.Distinct().ToListAsync());
+
+            var TodayDateTime = DateTime.Today;
             CurrentDate = TodayDateTime.ToShortDateString();
-            Computer = await _context.Computer.ToListAsync();
+            Computer = await computers.ToListAsync();
         }
     }
 }
