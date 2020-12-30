7a72ecfacebffe0d3fb0e5b8f570b86521e043a6
Joshua Ramczyk <joswar84@gmail.com>
2020-12-30 13:15:38 -0600

    logout to index

---
+++ Areas/Identity/Data/JGR_WGU_CapstoneContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JGR_WGU_Capstone.Data
+{
+    public class JGR_WGU_CapstoneContext : IdentityDbContext<IdentityUser>
+    {
+        public JGR_WGU_CapstoneContext(DbContextOptions<JGR_WGU_CapstoneContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
